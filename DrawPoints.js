caf47a0c44aefbbf59b71a19e248e74887836896
harpreetdev01 <harpreet.dev01@gmail.com>
2024-11-01 09:02:58 -0400

    Create a point via clicking a plane mesh

--- DrawPoints.js
+++ DrawPoints.js
@@ -2,7 +2,7 @@ import './style.css'
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
-let scene, camera, renderer, cubeGeo, planeGeo;
+let scene, camera, renderer, planeGeo;
 const webglCanvas = document.querySelector('.webgl');
 
 // ------------------------- SET UP ------------------------//
@@ -23,24 +23,60 @@ renderer = new THREE.WebGLRenderer(
 );
 renderer.setSize(window.innerWidth, window.innerHeight);
 
-// const controls = new OrbitControls(camera, renderer.domElement);
-// controls.update();
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.update();
 
 // Plane
 planeGeo = new THREE.PlaneGeometry(20, 20);
 const planeMaterial = new THREE.MeshBasicMaterial({ color: 0x33ff99});
 const plane = new THREE.Mesh(planeGeo, planeMaterial);
-plane.rotation.x = -Math.PI / 2;
-plane.position.y = -1;
-// scene.add(plane);
+scene.add(plane);
+
+// Raycaster
+const raycaster = new THREE.Raycaster();
+const mouse = new THREE.Vector2();
+// const pointMaterial = new THREE.PointsMaterial({size: 2, color: 0xffff00 });
+
+let count = 1;
+
+
+window.addEventListener('click', (e) =>
+{
+    // Normalize mouse coordinates
+    mouse.x = (e.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = -(e.clientY / window.innerHeight) * 2 + 1;
+
+    raycaster.setFromCamera(mouse, camera);
+    const intersects = raycaster.intersectObject(plane);
+
+
+    if(intersects.length > 0)
+    {
+       const intersect = intersects[0];
+       console.log(intersect); 
+
+       intersect.point.z = 0;
+
+       const pointGeometry = new THREE.BufferGeometry();
+    //    pointGeometry.setAttribute('position', new THREE.Float32BufferAttribute([mouse.x, mouse.y, 0], 3));
+       pointGeometry.setAttribute('position', new THREE.Float32BufferAttribute(intersect.point.toArray(), 3));
+       const pointMaterial = new THREE.PointsMaterial({ size: 4.5, color: 0x00ff00 });
+       const point = new THREE.Mesh(pointGeometry, pointMaterial);
+       point.name = 'point'+count;
+       scene.add(point);
+
+       count++;
+    }
+
+})
 
 // Point
-const vectorA = new THREE.Vector3(0, 0, 0);
-const pointGeometry = new THREE.BufferGeometry();
-pointGeometry.setAttribute('position', new THREE.Float32BufferAttribute(vectorA, 3));
-const pointMaterial = new THREE.PointsMaterial({ size: 0.5, color: 0x00ff00 });
-const point = new THREE.Points(pointGeometry, pointMaterial);
-scene.add(point);
+// const vectorA = new THREE.Vector3(0, 0, 0);
+// const pointGeometry = new THREE.BufferGeometry();
+// pointGeometry.setAttribute('position', new THREE.Float32BufferAttribute(vectorA, 3));
+// const pointMaterial = new THREE.PointsMaterial({ size: 0.5, color: 0x00ff00 });
+// const point = new THREE.Points(pointGeometry, pointMaterial);
+// scene.add(point);
 
 
 
